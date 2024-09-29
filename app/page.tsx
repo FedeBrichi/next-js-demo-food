7f27856f4226099e8414ec626f49f0d4841595fc
FedeBrichi <zanellato.federico@gmail.com>
2024-09-29 15:32:24 +0200

    fix: some styles of the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -6,38 +6,44 @@ import styles from './page.module.css'
 // TODO think a better style
 export default function Home() {
   return (
-    <>
+    <div className={styles.wrapper}>
       <header className={styles.header}>
         <div className={styles.carousel}>
           <ImageCarousel />
         </div>
-        <div>
-          <div>
-            <h1>Some text</h1>
-            <p>Some text to explain stuff</p>
+        <div className={styles.info}>
+          <div className={styles.intro}>
+            <h1>Welcome in DA FUDDO!</h1>
+            <p>
+              Discover a world of flavors, creativity, and culinary inspiration
+              right at your fingertips. <br />
+              At DA FUDDO, we bring together food lovers, home cooks, and
+              professional chefs from all over the globe to share and explore
+              delicious recipes. Whether you're looking for a quick meal or a
+              gourmet dish, we've got something for every taste and occasion!
+            </p>
           </div>
-          <div>
+          <div className={styles.cta}>
             <Link href="/community">Join the Community</Link>
             <Link href="/meals">Check meals</Link>
           </div>
         </div>
       </header>
-      <main>
+      <main className={styles.main}>
         <section>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-          tristique turpis in urna placerat, quis bibendum nunc dignissim.
-          Curabitur ut dictum lectus, non pharetra nulla. Quisque sodales
-          consectetur molestie. Quisque est ex, lobortis ut orci ac, tincidunt
-          sollicitudin ex. Vestibulum in diam quis ipsum tempor cursus. Praesent
-          lobortis ut tortor ut pharetra. Cras mollis metus lectus, nec
-          vestibulum nibh volutpat id. Integer finibus erat a neque maximus
-          laoreet. Ut eget magna magna. Nunc et nibh sem. Nunc volutpat ante
-          libero, vitae ultricies nibh suscipit ac. Nulla leo lacus, tincidunt
-          et ullamcorper vestibulum, rutrum vel lorem. Vivamus in nisl sed nunc
-          ullamcorper interdum at sed sapien. Ut sed erat vitae augue ornare
-          lacinia at et elit. Maecenas eget efficitur purus. Aliquam turpis
+          What's Cooking? Explore Recipes: Browse our ever-growing collection of
+          recipes across different cuisines, diets, and ingredients. Find
+          something new to try every day! Create & Share: Have a recipe you're
+          proud of? Upload your own and share it with the community! Whether
+          it’s a family secret or your latest culinary experiment, we want to
+          see it. Food Stories: Every recipe has a story – read about the
+          origin, inspiration, and tips from fellow food enthusiasts. Join the
+          Community Sign up today to start uploading your favorite recipes, save
+          your top picks, and connect with a passionate community of food lovers
+          just like you. Whether you're a beginner in the kitchen or a seasoned
+          pro, DA FUDDO is the perfect place to explore your love for cooking.
         </section>
       </main>
-    </>
+    </div>
   )
 }
--- src/components/MainHeader/main-header.tsx
+++ src/components/MainHeader/main-header.tsx
@@ -3,32 +3,28 @@ import Link from 'next/link'
 
 import logoImg from '@/src/assets/images/logo.png'
 
-import MainHeaderBackground from './main-header-background'
 import styles from './main-header.module.css'
 import { NavLink } from '../NavLink'
 
 const MainHeader = () => {
   return (
-    <>
-      <MainHeaderBackground />
-      <header className={styles.wrapper}>
-        <Link href="/" className={styles.logo}>
-          <Image src={logoImg} alt="Bowl with veggies" priority />
-          <h1>Da Fuddo</h1>
-        </Link>
+    <header className={styles.wrapper}>
+      <Link href="/" className={styles.logo}>
+        <Image src={logoImg} alt="Bowl with veggies" priority />
+        <h1>Da Fuddo</h1>
+      </Link>
 
-        <nav className={styles.nav}>
-          <ul>
-            <li>
-              <NavLink href="/meals">Browse Meals</NavLink>
-            </li>
-            <li>
-              <NavLink href="/community">Join Community</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <NavLink href="/meals">Browse Meals</NavLink>
+          </li>
+          <li>
+            <NavLink href="/community">Community</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
